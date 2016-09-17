2372618fc1ad115676a7537135e5b52d79a2c9b4
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-16 19:55:09 -0500

    cleaned up code and added comments

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
+/********************************************************************************
+*                               Slasher Flick                                   *
+* Return the remaining elements of an array after chopping off n elements from  *
+* the head.                                                                     *
+********************************************************************************/
 function slasher(arr, howMany) {
+  // The splice() method changes the content of an array by removing existing
+  // elements and/or adding new elements
   arr.splice(0, howMany);
   return arr;
 }
 
+// Input Test Data
 console.log(slasher([1, 2, 3], 0));
-//console.log(slasher(["burgers", "fries", "shake"], 1));
-//console.log(slasher([1, 2, 3], 2));
-//console.log(slasher([1, 2, 3], 4));
-//console.log(slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5));
-//console.log(slasher([1, 2, 3], 9));
+console.log(slasher(["burgers", "fries", "shake"], 1));
+console.log(slasher([1, 2, 3], 2));
+console.log(slasher([1, 2, 3], 4));
+console.log(slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5));
+console.log(slasher([1, 2, 3], 9));
 
-
-
-/*
-var a = [1, 2, 3];
-console.log(a.splice(0,2)); //[1, 2]
-console.log(a); //[3] <------
-
-var b = [4, 5, 6];
-console.log(b.slice(1)); //[5, 6]
-console.log(b); //[4, 5, 6]
-var c = b.slice(1);
-console.log(c); //[5, 6]
-
-var d = [1, 2, 3];
-console.log(d.slice(0)); //[1, 2, 3]<------
-
-var e = [1, 2, 3];
-console.log(e.slice(9)); //[]
-console.log(e.splice(9)); //[]
-*/
+// Ooutput Data
+//[1, 2, 3]
+//["fries", "shake"]
+//[3]
+//[]
+//["cheese", 4]
+//[]
