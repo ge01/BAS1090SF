b3b53d823bf616645afbe3224074df7dd3391fa1
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-16 19:44:57 -0500

    working function

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-var x = [14, 3, 77, 44];
-/*
-console.log(x.slice(0, 1)); //[14]
-console.log(x.slice(1, 2)); //[3]
-console.log(x.slice(1)); //[3, 77, 44]
-console.log(x.slice(2)); //[77, 44]
-*/
-/*
-var y = x.slice(0,1);
-// Slice does not change the array which invokes it
-console.log(x); //[14, 3, 77, 44]
-console.log(y); //[14]
-*/
+function slasher(arr, howMany) {
+  arr.splice(0, howMany);
+  return arr;
+}
 
-// Alghough splice also takes two arguments (at minimum), the meaning is very different:
-// On top of that, splice mutates the array that calls it
-var z = x;
-var a = x;
-var b;
-/*
-console.log(z); //[14, 3, 77, 44]
-console.log(x.slice(1,2)); //[3]
-console.log(z.splice(1,2)); //[3, 77]
-console.log(z); //[14, 44]
-*/
+console.log(slasher([1, 2, 3], 0));
+//console.log(slasher(["burgers", "fries", "shake"], 1));
+//console.log(slasher([1, 2, 3], 2));
+//console.log(slasher([1, 2, 3], 4));
+//console.log(slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5));
+//console.log(slasher([1, 2, 3], 9));
 
-console.log(a); //[14, 3, 77, 44]
-console.log(x.slice(1,2)); //[3]
-b = a.splice(1,2);
-console.log(a); //[14, 44]
-console.log(b); //[3, 77]
+
+
+/*
+var a = [1, 2, 3];
+console.log(a.splice(0,2)); //[1, 2]
+console.log(a); //[3] <------
+
+var b = [4, 5, 6];
+console.log(b.slice(1)); //[5, 6]
+console.log(b); //[4, 5, 6]
+var c = b.slice(1);
+console.log(c); //[5, 6]
+
+var d = [1, 2, 3];
+console.log(d.slice(0)); //[1, 2, 3]<------
+
+var e = [1, 2, 3];
+console.log(e.slice(9)); //[]
+console.log(e.splice(9)); //[]
+*/
